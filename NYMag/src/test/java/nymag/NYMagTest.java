dc45f8861413233ab4e433a0044d0ca85a9f8b20
maisha-ahmed <maisha.ahmed64@gmail.com>
2020-01-21 23:15:17 -0500

    submitting 10 tests for nymag module

--- NYMag/src/test/java/nymag/NYMagTest.java
+++ NYMag/src/test/java/nymag/NYMagTest.java
@@ -2,12 +2,14 @@ package nymag;
 
 import common.CommonAPI;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class NYMagTest extends CommonAPI
 {
    @Test
-   public void testA()
+   public void testA()  //Just an example test
    {
       String url = "http://nymag.com/";
       driver.get(url);
@@ -41,4 +43,77 @@ public class NYMagTest extends CommonAPI
       driver.findElement(By.cssSelector(".auth0-label-submit")).click();
       Thread.sleep(3000);
    }
+   @Test
+   public void testLatestNews() throws InterruptedException
+   {
+      String url = "http://nymag.com/";
+      driver.get(url);
+      driver.findElement(By.cssSelector(".tab-trigger")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.cssSelector(".tab-trigger.intelligencer")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.cssSelector(".tab-trigger.wwwthecut")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.cssSelector(".tab-trigger.vulture")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.cssSelector(".tab-trigger.grubstreet")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.cssSelector(".tab-trigger.strategist")).click();
+      Thread.sleep(1000);
+   }
+   @Test
+   public void testThatBurgerIconAndSearchBarWorks() throws InterruptedException
+   {
+      String url = "http://nymag.com";
+      driver.get(url);
+      driver.findElement(By.cssSelector(".nav-dropdown-button-trigger")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.cssSelector(".nav-search-input")).sendKeys("Daniel Golden");
+      driver.findElement(By.cssSelector(".nav-search-submit-button")).click();
+      Thread.sleep(1000);
+   }
+   @Test
+   public void testGetTextFromDropDownMenu() throws InterruptedException
+   {
+      String url = "http://nymag.com";
+      driver.get(url);
+      driver.findElement(By.cssSelector(".nav-dropdown-button-trigger")).click();
+      System.out.println(driver.findElement(By.cssSelector(".dropdown-body-scroll")).getText());
+      Thread.sleep(1000);
+   }
+   @Test
+   public void testThatHelpLinkWorks() throws InterruptedException
+   {
+      String url = "http://nymag.com/";
+      driver.get(url);
+      driver.findElement(By.xpath("//a[text()='Help']")).click();
+      driver.findElement(By.id("query")).sendKeys("set up digital access");
+      driver.findElement(By.id("query")).sendKeys(Keys.ENTER);
+      Thread.sleep(3000);
+   }
+   @Test
+   public void testGetTextFromAboutUsPage()
+   {
+      String url = "http://nymag.com/";
+      driver.get(url);
+      driver.findElement(By.xpath("//a[text()='About Us']")).click();
+      System.out.println(driver.findElement(By.cssSelector(".layout-simple ")).getText());
+   }
+   @Test
+   public void testGetTextFromContactPage()
+   {
+      String url = "http://nymag.com/";
+      driver.get(url);
+      driver.findElement(By.xpath("//a[text()='Contact']")).click();
+      System.out.println(driver.findElement(By.cssSelector(".body")).getText());
+   }
+   @Test
+   public void testGetPrivacyNotice()
+   {
+      String url = "http://nymag.com/";
+      driver.get(url);
+      driver.findElement(By.xpath("//a[text()='Privacy']")).click();
+      System.out.println(driver.findElement(By.cssSelector(".body")).getText());
+   }
+
 }
